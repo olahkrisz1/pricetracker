38b6cdd3973900cfd66ede770f727e151feba98c
olahkrisz1 <olahkrisz1@gmail.com>
2023-10-09 19:51:14 +0200

    parsing info

--- lib/actions/scraper/index.ts
+++ lib/actions/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+import { extractCurrency, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
@@ -43,7 +43,37 @@ export async function scrapeAmazonProduct(url: string) {
         $('#priceblock_dealprice'),
         $('.a-size-base.a-color-price')
     )
-    console.log({title, currentPrice, originalPrice})
+
+    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+    
+    const images  = 
+    $('#imgBlkFront').attr('data-a-dynamic-image') || 
+    $('#landingImage').attr('data-a-dynamic-image') ||
+    '{}'
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($('.a-price-symbol'))
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g,"");
+
+    // construct data object with scraped information
+    const data = {
+        url,
+        currency: currency || '$',
+        image: imageUrls[0],
+        title,
+        currentPrice: Number(currentPrice),
+        originalPrice: Number(originalPrice),
+        priceHistory: [],
+        discountRate: Number(discountRate),
+        category: 'category',
+        reviewsCount: 100,
+        stars: 4.5,
+        isOutOfStock: outOfStock,
+
+
+    }
+    console.log(data);
 
    } catch(error:any) {
        throw new Error(`Failed to scrape product: ${error.messsage}`)
