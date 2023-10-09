e552771cea48148e21ad01e180bd21930e362dec
olahkrisz1 <olahkrisz1@gmail.com>
2023-10-09 18:02:40 +0200

    commit

--- lib/actions/scraper/index.ts
+++ lib/actions/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
@@ -24,8 +25,25 @@ export async function scrapeAmazonProduct(url: string) {
    try {
     // Fetch the product page
     const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data)
+    
+    // Extract the product title
+    const title = $('#productTitle').text().trim();
+    const currentPrice = extractPrice(
+        $('.priceToPay span.a-price-whole'),
+        $('a.size.base.a-color-price'),
+        $('.a-button-selected .a-color-base'),
+        $('.a-price.a-text-price')
+    );
 
-    console.log(response.data)
+    const originalPrice = extractPrice(
+        $('#priceblock_ourprice'),
+        $('.a-price.a-text-price span.a-offscreen'),
+        $('#listPrice'),
+        $('#priceblock_dealprice'),
+        $('.a-size-base.a-color-price')
+    )
+    console.log({title, currentPrice, originalPrice})
 
    } catch(error:any) {
        throw new Error(`Failed to scrape product: ${error.messsage}`)
